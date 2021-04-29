e9cbfb261f33abc928db4a738cfe80debf540d40
Miguel Angel Jimenez <miguejm@outlook.es>
2021-04-29 10:42:11 -0500

    Clase container, mx-auto:para centrar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tailwindcss</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+    <style>
+        /* Comprobar que la clase container si funciona */
+        .container{
+            background-color: cyan;
+        }
+    </style>
 </head>
 <body>
-    <h1>Este es un H1</h1>
-    <h2>Este es un H2</h2>
-    <h3>Este es un H3</h3>
-
-
+    <div class="container mx-auto">
+        <h1>Este es un H1</h1>
+        <h2>Este es un H2</h2>
+        <h3>Este es un H3</h3>
+    </div>
     <script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
