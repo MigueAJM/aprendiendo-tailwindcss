09d36e5889ba887024ebbe21618b85c5064d8bc0
Miguel Angel Jimenez <miguejm@outlook.es>
2021-04-29 13:59:26 -0500

    Tipografia: class leading para interliniado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     <div class="container">
         <h1 class="font-sans text-6xl md:text-3xl font-bold">Este es un titulo</h1>
         <p class="font-serif">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, fugiat modi cupiditate nostrum, deleniti iste, dolorem earum placeat distinctio dicta qui tempora aliquid! Accusantium possimus officia nulla cum delectus voluptate?</p>
-        <ul class="font-mono text-sm italic">
+        <ul class="font-mono text-sm italic leading-relaxed">
             <li>Elemento #1</li>
             <li>Elemento #2</li>
             <li>Elemento #3</li>
